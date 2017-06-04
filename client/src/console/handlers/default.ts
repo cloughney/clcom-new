c7a340d3f90761166ac3fe93d5f104314e2524fb
Chris Loughney <chris@chrisloughney.com>
2017-06-04 13:36:20 -0700

    update console commands

--- client/src/console/handlers/default.ts
+++ client/src/console/handlers/default.ts
@@ -1,28 +1,46 @@
 import { ConsoleTextLineType } from '../components/console';
 
-export default function onCommandReceived(command: ConsoleCommand): Promise<number> {
-	let exe = commands.get(command.name) || exeNotFound;
-	return exe(command);
+interface ConsoleCommandExecutable {
+	name: string;
+	description: string;
+	exe: (receivedCommand: ConsoleCommand) => Promise<number>;
 }
+
+const commands = new Map<string, ConsoleCommandExecutable>();
+commands.set('help', {
+	name: 'help',
+	description: 'Displays a list of available commands.',
+	exe: cmd => new Promise<number>(resolve => {
+		cmd.stdout.writeLine('Available commands:');
+		commands.forEach((command, commandName) => {
+			cmd.stdout.writeLine(` > ${commandName} - ${command.description}`);
+		});
+
+		resolve(0);
+	})
+});
+
+commands.set('clear', {
+	name: 'clear',
+	description: 'Clears all previous output from the console window.',
+	exe: async cmd => {
+		await cmd.stdout.clear();
+		return Promise.resolve(0);
+	}
+});
+
+const exeNotFound = async (cmd: any): Promise<number> => {
+	await cmd.stderr.writeLine(`'${cmd.name}' is not a valid command.`);
+	return Promise.resolve(1);
+};
 
 export const welcomeMessage = [
 	{ text: 'Welcome!', type: ConsoleTextLineType.Standard, hasEol: true },
 	{ text: 'Type \'help\' to see available commands.', type: ConsoleTextLineType.Standard, hasEol: true }
 ];
 
-const commands = new Map<string, (command: any) => Promise<number>>();
-commands.set('help', cmd => new Promise<number>(resolve => {
-	cmd.stdout.write('HELP!');
-	cmd.stdout.write('HELP!');
-	resolve(0);
-}));
-
-commands.set('clear', async cmd => {
-	await cmd.stdout.clear();
-	return Promise.resolve(0);
-});
-
-const exeNotFound = async (cmd: ConsoleCommand) => {
-	await cmd.stderr.writeLine(`'${cmd.name}' is not a valid command.`);
-	return Promise.resolve(1);
-};
+export default function onCommandReceived(command: ConsoleCommand): Promise<number> {
+	const matchedCommand = commands.get(command.name);
+	const exe = matchedCommand ? matchedCommand.exe : exeNotFound;
+	return exe(command);
+}
