682ad423c3f1671e8fc094c5351d5dcbf5b5bd15
Chris Loughney <chris@chrisloughney.com>
2017-09-14 23:50:43 -0700

    move window manager into new repo

--- client/src/showcase/components/activity-window/index.tsx
+++
@@ -1,4 +0,0 @@
-import ActivityWindow from './activity-window';
-export * from './activity-window';
-export * from './types';
-export default ActivityWindow;
--- client/src/showcase/index.ts
+++ client/src/showcase/index.ts
@@ -1,5 +1,45 @@
-import WindowManager from './containers/window-manager';
+import { Action, Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { AppState, actionFactory } from './support';
 
-export * from './containers/window-manager';
-export * from './support';
-export default WindowManager;
+function mapStateToProps(state: AppState): Partial<Props> {
+	return {
+		availableActivities: state.availableActivities,
+		openWindows: state.openWindows
+	};
+}
+
+function mapDispatchToProps(dispatch: Dispatch<Action>): Partial<Props> {
+	return {
+		onWindowAction: (action, window, options = {}) => {
+			switch (action) {
+				case WindowAction.Open:
+					dispatch(actionFactory.openWindow(options.activity));
+					break;
+				case WindowAction.Close:
+					dispatch(actionFactory.closeWindow(options.window || window));
+					break;
+				case WindowAction.Focus:
+					dispatch(actionFactory.focusWindow(options.window || window));
+					break;
+				case WindowAction.Move:
+					dispatch(actionFactory.moveWindow(options.window || window, { x: options.x, y: options.y }));
+					break;
+				case WindowAction.Resize:
+					dispatch(actionFactory.resizeWindow(options.window || window));
+					break;
+				case WindowAction.Maximize:
+					dispatch(actionFactory.maximizeWindow(options.window || window));
+					break;
+				case WindowAction.Minimize:
+					dispatch(actionFactory.minimizeWindow(options.window || window));
+					break;
+				case WindowAction.Restore:
+					dispatch(actionFactory.restoreWindow(options.window || window));
+					break;
+				default:
+					throw new Error('Invalid action type performed on window.');
+			}
+		}
+	};
+}
